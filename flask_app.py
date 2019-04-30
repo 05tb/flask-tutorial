50060d48a3a9d7f36f1d9cfbdaafec25777e93c1
Todd Burlington <05tb@users.noreply.github.com>
2019-04-30 11:25:04 +0100

    pandas

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,22 @@
 # flask_app.py
-from flask import Flask
+import pandas as pd
+from flask import Flask, jsonify
 app = Flask(__name__)
 app.config["DEBUG"] = True
+
 
 @app.route("/")
 def hello():
     return "Hello!"
 
+
 @app.route("/bye")
 def bye():
     return "Bye!"
+
+
+@app.route("/api")
+def data():
+    d = {'col1': [1, 2], 'col2': [3, 4]}
+    df = pd.DataFrame(data=d)
+    return jsonify(df.to_json())
