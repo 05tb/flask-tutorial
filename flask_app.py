38f72575e25f5d0dbc7e349e0e28d4d07ed79836
Todd Burlington <05tb@users.noreply.github.com>
2019-04-30 15:09:49 +0100

    Database

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,27 @@
 # flask_app.py
+import config
 import pandas as pd
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
-app.config["DEBUG"] = True
+
+# Set debug status
+if config.ENVIRONMENT == 'development':
+    app.config["DEBUG"] = True
+else:
+    app.config["DEBUG"] = False
+
+# Set db config
+for key, value in config.DB[config.ENVIRONMENT].items():
+    app.config[key] = value
+db = SQLAlchemy(app)
+
+
+class Api(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    request = db.Column(db.Text, nullable=False)
+    response = db.Column(db.Text, nullable=False)
 
 
 @app.route("/")
@@ -18,8 +37,14 @@ def bye():
 @app.route("/api", methods=["GET", "POST"])
 def data():
     if request.method == "GET":
+        api_request = Api(request=request.method, response="Post some JSON data here!")
+        db.session.add(api_request)
+        db.session.commit()
         return "Post some JSON data here!"
 
     if request.method == "POST":
         df = request.get_json(force=True)
+        api_request = Api(request=request.method, response=str(df))
+        db.session.add(api_request)
+        db.session.commit()
         return jsonify(df)
