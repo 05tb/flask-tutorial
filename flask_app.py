966cee5d11c0d4090bec607e64095e94ef4c83d2
Todd Burlington <05tb@users.noreply.github.com>
2019-05-01 14:24:14 +0100

    Prediction end point

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,10 @@
 # flask_app.py
 import config
+import pickle
 import pandas as pd
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 app = Flask(__name__)
 
@@ -20,6 +22,8 @@ db = SQLAlchemy(app)
 
 class Api(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    route = db.Column(db.Text, nullable=False)
+    method = db.Column(db.Text, nullable=False)
     request = db.Column(db.Text, nullable=False)
     response = db.Column(db.Text, nullable=False)
 
@@ -37,14 +41,50 @@ def bye():
 @app.route("/api", methods=["GET", "POST"])
 def data():
     if request.method == "GET":
-        api_request = Api(request=request.method, response="Post some JSON data here!")
+        api_request = Api(route="/api",
+                          method=request.method,
+                          request=request.method,
+                          response="Post some JSON data here!")
         db.session.add(api_request)
         db.session.commit()
         return "Post some JSON data here!"
 
     if request.method == "POST":
         df = request.get_json(force=True)
-        api_request = Api(request=request.method, response=str(df))
+        api_request = Api(route="/api",
+                          method=request.method,
+                          request=str(request.get_json(force=True)),
+                          response=str(df))
         db.session.add(api_request)
         db.session.commit()
         return jsonify(df)
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    user_input = request.get_json(force=True)
+    user_input = list(user_input)
+    print(user_input)
+
+    model = pickle.load(open("model.pickle", "rb"))
+    vectorizer = pickle.load(open("vectorizer.pickle", "rb"))
+
+    X_user = pd.DataFrame(vectorizer.transform(user_input).toarray(),
+                          columns=vectorizer.get_feature_names())
+
+    prediction = model.predict(X_user)
+    prediction = prediction.tolist()
+
+    api_request = Api(route="/predict",
+                      method=request.method,
+                      request=str(user_input),
+                      response=str(prediction))
+    db.session.add(api_request)
+    db.session.commit()
+
+    return jsonify(prediction)
+
+
+# @app.route("/train", methods=["POST"])
+# def train():
+#     return 
